02699d146326023c1dabe6c53f4d4c92d0c6abb0
xDavid-Gabriel <davidbetalleluz31@gmail.com>
2021-08-29 22:15:31 -0500

    sunnyside

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+let btnMenu = document.getElementById("btn-menu");
+let menu = document.getElementById("menu");
+
+btnMenu.addEventListener("click", function(){
+    menu.classList.toggle("mostrar");
+
+})
